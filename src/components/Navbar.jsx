1086d7bb6897f61cae9cde20994e17cd1672d507
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-04-18 00:23:57 +0800

    add functionality to the Navbar and make responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,66 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../assets/design-build-deploy-logo.png";
+import { FaBars, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggleHamburger = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img
-              className="h-14 w-14 mr-2"
+              className="h-16 w-16 mr-3"
               src={logo}
               alt="Design Build and Deploy Logo"
             />
-            <span className="text-lg text-white tracking-tight">
+            <span className="text-sm text-white tracking-wide">
               Design Build Deploy
             </span>
           </div>
+          <ul className="hidden lg:flex ml-14 space-x-12 text-sm text-white">
+            <li className="px-2">About</li>
+            <li className="px-2">Portfolio</li>
+            <li className="px-2">Contact</li>
+          </ul>
+          <div className="hidden lg:flex justify-center items-center space-x-12">
+            <button
+              type="button"
+              className="bg-yellow-400 px-5 py-1 text-center font-bold rounded-md cursor-pointer  2008"
+            >
+              Download CV
+            </button>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button type="button" onClick={toggleHamburger}>
+              {mobileMenuOpen ? (
+                <FaXmark size={24} color="white" />
+              ) : (
+                <FaBars size={24} color="white" />
+              )}
+            </button>
+          </div>
         </div>
+        {mobileMenuOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul className="text-sm text-center text-white p-2">
+              <li className="my-2">About</li>
+              <li className="my-2">Portfolio</li>
+              <li className="my-2">Contact</li>
+            </ul>
+            <button
+              type="button"
+              className="bg-yellow-400 px-5 py-1 text-center font-bold rounded-md cursor-pointer  2008"
+            >
+              Download CV
+            </button>
+          </div>
+        )}
       </div>
     </nav>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-heroImage bg-auto bg-no-repeat bg-center"></div>
+    </>
+  );
+};
+
+export default Home;
