ca3bfe5dcb95abbb3d4628bed265cadd0c7141b4
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-04-22 21:52:55 +0800

    added hero image and adjusted image size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { FaGithub, FaCodepen } from "react-icons/fa";
 import { BsLinkedin } from "react-icons/bs";
+import heroImage from "../assets/hero-image.png";
 
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div
-          className="bg-cover bg-center h-80 w-96"
-          style={{
-            backgroundImage: "url('/assets/hero-image.png')",
-          }}
-        ></div>
-      </div>
-      <div className="flex flex-col justify-center items-center mt-10 px-2 hover:animate-pulse">
-        <h1 className="text=2x1 text-[#C9C9C9] text-center font-medium tracking-wide">
-          &lt; <span className="text-3xl font-semibold">SHANE KENZLER</span>{" "}
-          Frontend Developer &#47;&gt;
-        </h1>
-        <p className="text-lg  text-[#C9C9C9] font-light">
-          I design, build and deploy scalable web apps
-        </p>
-      </div>
+      <div className="flex justify-around items-center p-2">
+        <div className="flex flex-col justify-center items-center px-2">
+          <h1 className="text-2x1 text-[#C9C9C9] text-center font-medium tracking-wide">
+            &lt; <span className="text-3xl font-semibold">SHANE KENZLER</span>{" "}
+            Frontend Developer &#47;&gt;
+          </h1>
+          <p className="text-lg  text-[#C9C9C9] font-light">
+            I design, build and deploy scalable web apps
+          </p>
 
-      <div className="flex justify-center items-center mt-10">
-        <button
-          type="button"
-          className="flex justify-center items-center border-2  border-yellow-400 px-5 py-2 text-yellow-400 text-center text-base tracking-wide gap-4 font-normal rounded-md cursor-pointer hover:border-[#C9C9C9] hover:text-[#C9C9C9]"
-        >
-          <FaGithub size={25} />
-          Checkout my Github
-        </button>
+          <button
+            type="button"
+            className="flex justify-between items-center border-2  border-yellow-400 px-5 py-2 text-yellow-400 text-center text-base tracking-wide gap-4 font-normal rounded-md mt-10 cursor-pointer hover:border-[#C9C9C9] hover:text-[#C9C9C9]"
+          >
+            <FaGithub size={25} />
+            Checkout my Github
+          </button>
+        </div>
+
+        <img
+          className="flex justify-end items-end w-3/5"
+          src={heroImage}
+          alt="Profile Hero Image"
+        />
       </div>
-      <div className="flex justify-center items-center gap-4 hover:animate-pulse">
+      <div className="flex justify-center items-center gap-4 mt-10 hover:animate-pulse">
         <FaGithub size={24} color="#C9C9C9" />
         <BsLinkedin size={24} color="#C9C9C9" />
         <FaCodepen size={24} color="#C9C9C9" />
