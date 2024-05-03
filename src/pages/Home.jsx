d9a0c607c0b7a8afa81477c17ad5882a09375990
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-05-03 18:33:51 +0800

    Added animation elements using Framer Motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { FaGithub, FaCodepen } from "react-icons/fa";
 import { BsLinkedin } from "react-icons/bs";
 import heroImage from "../assets/hero-image.png";
+import { easeIn, easeInOut, motion } from "framer-motion";
 
 const Home = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-center items-center text-center ">
-        <div className="w-3/4 flex flex-col justify-center items-center text-[#C9C9C9] mt-5 p-5">
+        <motion.div
+          className="w-3/4 flex flex-col justify-center items-center text-[#C9C9C9] mt-5 p-5"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 3, ease: easeIn }}
+        >
           <h1 className="text-5xl sm:text-6xl lg:text-7xl font-medium tracking-wide p-2">
             Frontend <span className="font-black uppercase">Developer</span>
           </h1>
@@ -26,14 +32,19 @@ const Home = () => {
             <FaGithub size={25} />
             Checkout my Github
           </button>
-        </div>
-        <div className="flex justify-end items-center">
+        </motion.div>
+        <motion.div
+          className="flex justify-end items-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 1, ease: easeIn }}
+        >
           <img
             className="w-full object-fill flex-shrink-0"
             src={heroImage}
             alt="Profile Hero Image"
           />
-        </div>
+        </motion.div>
       </div>
       <div className="flex justify-center items-center gap-8 m-5">
         <div className="hover:scale-125 cursor-pointer">
