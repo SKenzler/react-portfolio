0d14c6f3c62e8e46431bcf82b5672b7525184990
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-05-06 22:49:24 +0800

    add dot indicators to the ProjectCarousel

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { FaCaretLeft, FaCaretRight } from "react-icons/fa6";
-
+import { FaCaretLeft, FaCaretRight, FaCircle } from "react-icons/fa6";
 import { motion, AnimatePresence } from "framer-motion";
 
 const ProjectCarousel = ({ images }) => {
@@ -17,16 +16,17 @@ const ProjectCarousel = ({ images }) => {
     setCurrentIndex((previousIndex) =>
       previousIndex - 1 < 0 ? images.length - 1 : previousIndex - 1
     );
-
-    const handleDotClick = (index) => {
-      setCurrentIndex(index);
-    };
   };
+
+  const handleDotClick = (index) => {
+    setCurrentIndex(index);
+  };
+
   return (
-    <div className="w-full flex flex-col justify-center items-center p-10 mt-5">
+    <div className="w-full flex flex-col justify-center items-center mt-5">
       <img
         key={currentIndex}
-        className="flex justify-center items-end w-6/12 object-fill p-2"
+        className="flex justify-center items-cneter w-5/6 sm:w-6/12 object-fill p-2"
         src={images[currentIndex]}
         alt="Left Arrow"
       />
@@ -37,6 +37,27 @@ const ProjectCarousel = ({ images }) => {
           onClick={handlePrevious}
         >
           <FaCaretLeft size={24} />
+        </div>
+        <div className="flex justify-center items-center text-[#C9C9C9]">
+          {images.map((_, index) =>
+            index === currentIndex ? (
+              <div
+                key={index}
+                className="hover:text-yellow-400 p-2 cursor-pointer"
+                onClick={console.log(index, currentIndex)}
+              >
+                <FaCircle color="yellow" size={10} />
+              </div>
+            ) : (
+              <div
+                key={index}
+                className="hover:text-yellow-400 p-2 cursor-pointer"
+                onClick={console.log(index, currentIndex)}
+              >
+                <FaCircle size={10} />
+              </div>
+            )
+          )}
         </div>
         <div
           className="hover:text-yellow-400 cursor-pointer"
