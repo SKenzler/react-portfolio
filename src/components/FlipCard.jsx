bb5e6c21894bb451fa0a2db3946db0333b866147
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-05-23 11:04:19 +0800

    Building the FlipCard component to pull data from a cards object

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-import reactToDo from "../assets/react-to-do-list.jpg";
-import { FaGithub } from "react-icons/fa";
+import cards from "../utilities/cards";
 
-const FlipCard = () => {
+const FlipCard = (cards) => {
   return (
     <div className="flex flex-col min-h-screen justify center items-center bg-slate-100 p-10">
       <div className="group h-80 w-80 [perspective:1000px]">
         <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          {cards.map((item,index) =>  (
           <div className="absolute inset-0">
             <img
-              src={reactToDo}
+              src={cards.image}
               alt=""
               className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
             />
           </div>
           <div className="absolute inset-0 h-full w-full rounded-xl bg-black px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
             <div className="flex min-h-full flex-col justify-center items-center">
-              <h1 className="text-3xl font-bold p-2">React To Do List</h1>
-              <p className="text-base">
-                An interactive To Do List built using React. The To Do List
-                dynamically populates as more tasks are added.
-              </p>
+              <h1 className="text-3xl font-bold p-2"> {cards.header}</h1>
+              <p className="text-base">{cards.header}</p>
               <button
                 type="button"
                 className="text-yellow-400 hover:text-[#C9C9C9] p-2"
               >
-                <FaGithub size={24} />
+                {cards.icon}
               </button>
             </div>
           </div>
+          ))};
         </div>
       </div>
     </div>
---
+++ src/utilities/cards.jsx
@@ -0,0 +1,15 @@
+import cardImage from "../assets/react-to-do-list.jpg";
+import { FaGithub } from "react-icons/fa";
+
+const cards = [
+  {
+    id: Date.now().toString(36) + Math.random().toString(36),
+    image: cardImage,
+    header: "React To Do List",
+    content:
+      "An interactive To Do List in React. The To Do List dynamically populates as more tasks are added. Additionlly, a user can edit and delete a task",
+    icon: <FaGithub size={28} />,
+  },
+];
+
+export default cards;
