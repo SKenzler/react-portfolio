f0678bd96b4bc49770fe25bc5268d78789c9790d
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-05-24 18:20:58 +0800

    Improved the styling on the FlipCard content

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const FlipCard = ({ cards }) => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
       {cards.map((item) => (
         <div
           key={item.id}
-          className="flex flex-col min-h-screen justify center items-center bg-slate-100 p-10"
+          className="flex flex-col min-h-screen justify-center items-center"
         >
-          <div className="group h-80 w-80 [perspective:1000px]">
-            <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          <div className="group h-80 w-64 [perspective:1000px]">
+            <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-1000 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
               <div className="absolute inset-0">
                 <img
                   src={item.image}
@@ -17,13 +17,15 @@ const FlipCard = ({ cards }) => {
                   className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
                 />
               </div>
-              <div className="absolute inset-0 h-full w-full rounded-xl bg-black px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <div className="absolute inset-0 h-full w-full rounded-xl bg-black px-3 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
                 <div className="flex min-h-full flex-col justify-center items-center">
-                  <h1 className="text-2xl font-bold p-2">{item.header}</h1>
-                  <p className="text-base">{item.content}</p>
+                  <h1 className="text-lg font-semibold p-2">{item.header}</h1>
+                  <p className="text-sm leading-relaxed font-light text-center text-pretty py-2">
+                    {item.content}
+                  </p>
                   <button
                     type="button"
-                    className="text-yellow-400 hover:text-[#C9C9C9] p-2"
+                    className="text-yellow-400 hover:text-[#C9C9C9] p-4"
                   >
                     {item.icon}
                   </button>
--- src/utilities/cards.jsx
+++ src/utilities/cards.jsx
@@ -1,22 +1,41 @@
 import cardImageOne from "../assets/react-to-do-list.jpg";
+import cardImageTwo from "../assets/react-profile-card.jpg";
+import cardImageThree from "../assets/theme-toggler-sreenshot.jpg";
+import cardImageFour from "../assets/react-password-generator.jpg";
 import { FaGithub } from "react-icons/fa";
 
 const cards = [
   {
     id: Date.now().toString(36) + Math.random().toString(36),
     image: cardImageOne,
-    header: "React To Do List",
+    header: "React To-Do List",
     content:
-      "An interactive To Do List in React. The To Do List dynamically populates as more tasks are added. Additionlly, a user can edit and delete a task",
-    icon: <FaGithub size={28} />,
+      "An interactive To-Do List that dynamically populates as the user adds tasks. Additionlly, a user can edit and delete a task.",
+    icon: <FaGithub size={42} />,
   },
   {
     id: Date.now().toString(36) + Math.random().toString(36),
-    image: cardImageOne,
+    image: cardImageTwo,
     header: "React Profile Card",
     content:
-      "An interactive To Do List in React. The To Do List dynamically populates as more tasks are added. Additionlly, a user can edit and delete a task",
-    icon: <FaGithub size={28} />,
+      "An interactive profile card that features an active state on hover, expanding to reveal additional content.",
+    icon: <FaGithub size={42} />,
+  },
+  {
+    id: Date.now().toString(36) + Math.random().toString(36),
+    image: cardImageThree,
+    header: "React Theme Toggler",
+    content:
+      "An app that enables a user to toggle between dark and light mode themes.",
+    icon: <FaGithub size={42} />,
+  },
+  {
+    id: Date.now().toString(36) + Math.random().toString(36),
+    image: cardImageFour,
+    header: "React Password Generator",
+    content:
+      "An app that enables a user to generate a random password based on specific selection criteria and copy it to the clipboard.",
+    icon: <FaGithub size={42} />,
   },
 ];
 
