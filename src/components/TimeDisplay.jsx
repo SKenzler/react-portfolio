4921bdf25a5b2136d9a0e678ec7bd690ffcfe7cb
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-05-30 16:06:24 +0800

    Finalised TimeDisplay component and added it to the NavBar

--- src/components/TimeDisplay.jsx
+++ src/components/TimeDisplay.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const TimeDisplay = () => {
-  const date = new Date();
-  const showTime =
-    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  return <div className="text-[#C9C9C9] text-sm">{showTime}</div>;
-};
+  const [date, setDate] = useState(new Date());
 
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <div className="w-10 text-[#C9C9C9] text-sm">
+      {date.toLocaleTimeString()}
+    </div>
+  );
+};
 export default TimeDisplay;
