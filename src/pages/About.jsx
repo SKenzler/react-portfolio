5c309e03eae4a12f3e3a048d126472244a140847
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-06-04 12:29:33 +0800

    Change the layout of the About Cards

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -41,9 +41,12 @@ const About = () => {
         </div>
         <SkillSlider />
         <div className="w-full flex flex-col justify-start items-center p-2">
-          <div className="w-1/2 flex flex-col sm:flex-row border border-[#C9C9C9] rounded-lg shadow-lg my-10">
+          <div className="w-72 sm:w-1/4 flex flex-col border border-yellow-400 rounded-lg shadow-lg my-10">
+            <h1 className="bg-yellow-400 text-3xl text-[#101824] text-center uppercase font-bold rounded-t-lg-lg py-2">
+              Design
+            </h1>
             <motion.div
-              initial={{ opacity: 0, x: -50 }}
+              initial={{ opacity: 0, x: -20 }}
               whileInView={{
                 opacity: 1,
                 x: 0,
@@ -52,41 +55,21 @@ const About = () => {
               viewport={{ once: true }}
             >
               <img
-                className="w-3/4 sm:w-full object-fill"
+                className="w-50 object-fill"
                 src={aboutDesign}
                 alt="Portfolio Landing Pages with Figma logo"
               />
             </motion.div>
-            <div className="w-full sm:w-4/12 flex flex-col justify-start items-start text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
-              <h1 className="text-3xl sm:text-4xl text-start uppercase font-bold py-2">
-                Design
-              </h1>
+            <div className="w-full flex justify-start items-start text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
               <p>Using Figma, I design wireframes, compnents and protypes</p>
             </div>
           </div>
-          <div className="w-1/2 flex border border-[#C9C9C9] rounded-lg shadow-lg my-10">
+          <div className="w-72 sm:w-1/4 flex flex-col border border-yellow-400 rounded-lg shadow-lg my-10">
+            <h1 className="bg-yellow-400 text-3xl text-[#101824] text-center uppercase font-bold rounded-t-lg-lg py-2">
+              Build
+            </h1>
             <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: { duration: 1, delay: 1 },
-              }}
-              viewport={{ once: true }}
-            >
-              <img src={aboutBuild} alt="Vscode logo with code examples" />
-            </motion.div>
-            <div className="w-4/12 text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
-              <h1 className="text-2xl uppercase font-bold py-2">Build</h1>
-              <p>
-                Build using VsCode as an EDI React JS, Tailwind CSS and Framer
-                Motion dependncies.
-              </p>
-            </div>
-          </div>
-          <div className="flex my-10">
-            <motion.div
-              initial={{ opacity: 0, x: -50 }}
+              initial={{ opacity: 0, x: -20 }}
               whileInView={{
                 opacity: 1,
                 x: 0,
@@ -95,13 +78,36 @@ const About = () => {
               viewport={{ once: true }}
             >
               <img
-                src={aboutDeploy}
-                alt="React logo with examples of deployed apps"
+                className="w-50 object-fill"
+                src={aboutBuild}
+                alt="Portfolio Landing Pages with Figma logo"
               />
             </motion.div>
-            <div className="w-4/12 text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
-              <h1 className="text-2xl uppercase font-bold py-2">Deploy</h1>
-              <p>Deploy using GitHub pages</p>
+            <div className="w-full flex justify-start items-start text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
+              <p>Using Figma, I design wireframes, compnents and protypes</p>
+            </div>
+          </div>
+          <div className="w-72 sm:w-1/4 flex flex-col border border-yellow-400 rounded-lg shadow-lg my-10">
+            <h1 className="bg-yellow-400 text-3xl text-[#101824] text-center uppercase font-bold rounded-t-lg-lg py-2">
+              Deploy
+            </h1>
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+                transition: { duration: 1, delay: 1 },
+              }}
+              viewport={{ once: true }}
+            >
+              <img
+                className="w-50 object-fill"
+                src={aboutDeploy}
+                alt="Portfolio Landing Pages with Figma logo"
+              />
+            </motion.div>
+            <div className="w-full flex justify-start items-start text-sm text-[#C9C9C9] text-pretty tracking-wide p-5">
+              <p>Using Figma, I design wireframes, compnents and protypes</p>
             </div>
           </div>
         </div>
