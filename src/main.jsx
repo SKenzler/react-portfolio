d1055067607b9954545749cf386131485c7379c8
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-10-12 15:19:53 +0800

    add a useEffect Hook to track the ReactGA status

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { useEffect } from "react";
 import "./index.css";
 import ReactGA from "react-ga";
 
 ReactGA.initialize("G-BEF08EK3TL");
 
-ReactGA.pageview(window.location.pathname + window.location.search);
+useEffect(() => {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
