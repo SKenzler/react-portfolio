a5eddf035a48550e1b7b9b071e1473932929e697
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2024-10-28 20:20:43 +0800

    import useEffect to the About and Portfolio Page for use with ReactGA

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { easeIn, motion } from "framer-motion";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ProjectCarousel from "../components/ProjectCarousel";
